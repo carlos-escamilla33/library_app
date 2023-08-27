6f81641cb2a782354ceda45ef4f8801776f18623
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-08-26 22:45:21 -0700

    added title, add book btn, inputs

---
+++ script.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book() {
+    // the constructor
+}
+
+function addBookToLibrary() {
+    // takes user input and stores it into a variable
+    // takes title of book and store it in a variable
+    // takes author
+    // takes number of pages
+    // whether its been read or not
+}
+
+function outputBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(`these are the books ${myLibrary[i]}`);
+    }
+}
